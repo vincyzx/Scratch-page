cb598176ac1e8fd55b085c0da40565aa15724965
vincyzx <eriesivitin@gmail.com>
2024-09-20 16:08:45 -0300

    Add files via upload

---
+++ Fetec/index.js
@@ -0,0 +1,7 @@
+const nextButton = document.getElementById('btn-next')
+const cardContainer = document.getElementById('card-container')
+const gradient = document.getElementById('gradient')
+
+function next() {
+    alert('sdfafdfadsfa')
+}
